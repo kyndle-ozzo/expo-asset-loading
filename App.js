4208258303fe6beaa4a89606422472117e4f42de
Kyndle <kyndle@myceliuminc.net>
2023-12-08 18:15:09 -0800

    added fixAssetLoading function.

--- App.js
+++ App.js
@@ -11,6 +11,26 @@ export default function App() {
   );
 }
 
+const fixAssetLoading = () => {
+    // if (IF_ENV_WITH_UPDATES_ENABLED) {
+    //     setCustomSourceTransformer((resolver) => {
+    //         try {
+    //             // If Bundler is using the hashAssetFiles plugin if and only if the fileHashes property exists
+    //             if (resolver.asset.fileHashes) {
+    //                 const asset = Asset.fromMetadata(resolver.asset);
+    //                 return resolver.fromSource(asset.downloaded ? asset.localUri : asset.uri);
+    //             } else {
+    //                 return resolver.defaultAsset();
+    //             }
+    //         } catch {
+    //             return resolver.defaultAsset();
+    //         }
+    //     });
+    // }
+}
+
+fixAssetLoading();
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
