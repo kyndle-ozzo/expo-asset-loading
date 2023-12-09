987086cac4d620c3d9470346dd1354a594459549
Kyndle <kyndle@myceliuminc.net>
2023-12-08 18:03:43 -0800

    added image.

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={require("./assets/smash.jpg")} style={{width: 200, height: 200}}/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
